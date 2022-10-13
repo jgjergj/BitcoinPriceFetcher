e0b9e12489c26181a71848abfda35b46eebb1656
Gjergj Jorgji <gjergj.jorgji@gmail.com>
2022-10-13 23:32:59 +0200

    feat: changed DB from in memory to Sqlite to be able to prevent duplicates - added constrain to BitcoinPrice - added first migration

--- BitcoinPriceFetcher/Data/AppDbContext.cs
+++ BitcoinPriceFetcher/Data/AppDbContext.cs
@@ -5,9 +5,21 @@ namespace BitcoinPriceFetcher.Data
 {
     public class AppDbContext : DbContext, IAppDbContext
     {
-        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        public string DbPath { get; }
+        public AppDbContext()
         {
-            optionsBuilder.UseInMemoryDatabase(databaseName: "BitcoinPriceFetcher");
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "BitcoinPriceFetcher.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BitcoinPrice>()
+                .HasIndex(entity => new { entity.TimeStamp, entity.ProviderName }, "IDX_TimeAndProvider").IsUnique();
         }
 
         public DbSet<BitcoinPrice> BitcoinPrices { get; set; }
---
+++ BitcoinPriceFetcher/Data/Migrations/20221013212724_InitialMigration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BitcoinPriceFetcher.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BitcoinPrices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    TimeStamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ProviderName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BitcoinPrices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sources",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Endpoint = table.Column<string>(type: "TEXT", nullable: false),
+                    DocumentationLink = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sources", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IDX_TimeAndProvider",
+                table: "BitcoinPrices",
+                columns: new[] { "TimeStamp", "ProviderName" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BitcoinPrices");
+
+            migrationBuilder.DropTable(
+                name: "Sources");
+        }
+    }
+}
