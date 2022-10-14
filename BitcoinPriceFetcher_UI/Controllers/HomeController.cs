548e51e07da8767345c79618617701198e61a8e6
Gjergj Jorgji <gjergj.jorgji@gmail.com>
2022-10-14 11:15:00 +0200

    feat: added BitcoinPrice page

--- BitcoinPriceFetcher_UI/Controllers/HomeController.cs
+++ BitcoinPriceFetcher_UI/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text;
 
 namespace BitcoinPriceFetcher_UI.Controllers
 {
@@ -9,8 +10,11 @@ namespace BitcoinPriceFetcher_UI.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private static readonly HttpClient _httpClient = new HttpClient();
+
         public HomeController(ILogger<HomeController> logger)
         {
+            //_httpClient.BaseAddress = new Uri("https://localhost:5164/api/");
             _logger = logger;
         }
 
@@ -18,15 +22,66 @@ namespace BitcoinPriceFetcher_UI.Controllers
         {
             using var client = new HttpClient();
 
-            var resultString = await client.GetStringAsync("http://localhost:5164/api/sources");
-            List<Source> sources = JsonConvert.DeserializeObject<List<Source>>(resultString);
+            var response = await _httpClient.GetStringAsync("http://localhost:5164/api/sources");
+            //response.EnsureSuccessStatusCode();
+            //var resultString = await response.Content.ReadAsStringAsync();
+            List<Source> sources = JsonConvert.DeserializeObject<List<Source>>(response);
 
             return View(sources);
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> FetchPrice(string sourceName)
         {
-            return View();
+            //var requestContent = new StringContent(sourceName, Encoding.UTF8, "application/json");
+            //var response = await _httpClient.PostAsync("http://localhost:5164/api/bitcoinprice", requestContent);
+            //response.EnsureSuccessStatusCode();
+            //var content = await response.Content.ReadAsStringAsync();
+            //var createdBtcPrice = JsonConvert.DeserializeObject<BitcoinPrice>(content);
+
+
+
+
+
+            //URL
+            var url = "http://localhost:5164/api/bitcoinprice";
+
+            //Request
+            using var request = new HttpRequestMessage(HttpMethod.Post, url);
+
+            //Headers
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Add("Cache-Control", "no-cache");
+
+            //Payload
+            var payload = JsonConvert.SerializeObject(
+                new
+                {
+                    Name = sourceName
+                });
+            request.Content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            //Send
+            var response = await _httpClient.SendAsync(request);
+
+            //Handle response
+            //if (response.IsSuccessStatusCode)
+            var content = await response.Content.ReadAsStringAsync();
+            var createdBtcPrice = JsonConvert.DeserializeObject<BitcoinPrice>(content);
+
+            return Content(createdBtcPrice.DisplayPrice);
+        }
+
+        public async Task<IActionResult> BitcoinPrices()
+        {
+            using var client = new HttpClient();
+
+            var response = await _httpClient.GetStringAsync("http://localhost:5164/api/bitcoinprice");
+            //response.EnsureSuccessStatusCode();
+            //var resultString = await response.Content.ReadAsStringAsync();
+            List<BitcoinPrice> btcPrices = JsonConvert.DeserializeObject<List<BitcoinPrice>>(response);
+
+
+            return View(btcPrices);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
